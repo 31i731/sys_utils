d62bfbb55e4d18192ba085fad9c81f060b03a33c
31i731 <yougod871@gmail.com>
2019-11-06 16:42:27 +0100

    added a parameter to the function in order to indicate a path of pictures to choose from

--- random_background.py
+++ random_background.py
@@ -1,14 +1,8 @@
+import ctypes
 from random import choice as rnd_choice
 from os import listdir
-import ctypes
 
-img_path = "C:\\Users\\37255\\Pictures\\Saved Pictures\\"
-current_file = None
-
-def changeWallpaper():
-    rnd_file = rnd_choice([pic for pic in listdir(img_path) if not "ini" in pic and not pic == current_file])
-    global current_file
-    current_file = rnd_file
-    print(rnd_file)
-    #SPI_SETDESKWALLPAPER = 20 
-    #ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, img_path + rnd_file, 0)
+def changeWallpaper(toTakeFrom):
+    rnd_file = rnd_choice([pic for pic in listdir(toTakeFrom) if (".jpg" or ".png") in pic])
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, toTakeFrom + rnd_file, 0)
